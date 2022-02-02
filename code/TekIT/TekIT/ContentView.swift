6d25e20157f11cdcda3191959b0aa365592993ec
LuthforKhan <lrkhan@umich.edu>
2022-02-02 11:00:55 -0500

    added project and hifi

---
+++ code/TekIT/TekIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TekIT
+//
+//  Created by Luthfor Khan on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ code/TekIT/TekIT/TekITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TekITApp.swift
+//  TekIT
+//
+//  Created by Luthfor Khan on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TekITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
