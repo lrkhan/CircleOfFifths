e65b12d15fb6c75830f864b23462e45871195aef
LuthforKhan <lrkhan@umich.edu>
2022-02-07 11:41:30 -0500

    made new branch and new swiftUI file

---
+++ code/TekIT/TekIT/Screens/LaunchScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreen.swift
+//  TekIT
+//
+//  Created by Luthfor Khan on 2/7/22.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
