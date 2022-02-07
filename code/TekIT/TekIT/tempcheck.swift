7f58afc8132ca31dadc838c23e1d1cfc4bb93d00
Lamont Watkins <llwatkins81@gmail.com>
2022-02-07 09:20:07 -0500

    test

---
+++ code/TekIT/TekIT/tempcheck.swift
@@ -0,0 +1,20 @@
+//
+//  tempcheck.swift
+//  TekIT
+//
+//  Created by Lamont Watkins on 2/7/22.
+//
+
+import SwiftUI
+
+struct tempcheck: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct tempcheck_Previews: PreviewProvider {
+    static var previews: some View {
+        tempcheck()
+    }
+}
