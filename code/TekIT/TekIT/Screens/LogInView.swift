3d6705d6ae0d0403f3abd06a71c5db37b0669be1
Jerome <jlwarfield@aol.com>
2022-02-09 11:29:56 -0500

    up date login screen

--- code/TekIT/TekIT/Screens/LogInView.swift
+++ code/TekIT/TekIT/Screens/LogInView.swift
@@ -8,6 +8,66 @@
 import SwiftUI
 
 struct LogInView: View {
+    
+    struct ContentView: View {
+        @State private var name: String = ""
+        @State private var password: String = ""
+        let verticalPaddingForForm = 40.0
+        var body: some View {
+            ZStack {
+                
+                VStack(spacing: CGFloat(verticalPaddingForForm)) {
+                    Text("Welcome To TeckIT")
+                        .font(.title)
+                        .foregroundColor(Color.black)
+                    HStack {
+                        Image(systemName: "person")
+                            .foregroundColor(.secondary)
+                        TextField("Enter your name", text: $name)
+                            .foregroundColor(Color.black)
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    
+                    HStack {
+                        Image("icons8-key-50")
+                            .resizable()
+                            .frame(width: 16.0, height: 16.0) .foregroundColor(.secondary)
+                        SecureField("Enter password", text: $password)
+                            .foregroundColor(Color.black)
+                        
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Sign Up")
+                            .padding()
+                        
+                    }
+                    .background(Color.black)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+                    
+                }.padding(.horizontal, CGFloat(verticalPaddingForForm))
+                
+                struct ContentView: View {
+                               var body: some View {
+                                   SignInWithAppleButton(.signIn, onRequest: configure, onCompletion:handle)
+                                       .frame(height:45)
+                                       .padding()
+                               }
+                               }
+            }
+        };
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
