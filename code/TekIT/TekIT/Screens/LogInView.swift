1774ad80b3834a4232cc87a00709367d9b16f620
LuthforKhan <lrkhan@umich.edu>
2022-02-10 04:56:23 -0500

    re build Jerome's code for the vernal login view. old code commented out at bottom of file

--- code/TekIT/TekIT/Screens/LogInView.swift
+++ code/TekIT/TekIT/Screens/LogInView.swift
@@ -8,18 +8,26 @@
 import SwiftUI
 
 struct LogInView: View {
+    @State private var name: String = ""
+    @State private var password: String = ""
     
-    struct ContentView: View {
-        @State private var name: String = ""
-        @State private var password: String = ""
-        let verticalPaddingForForm = 40.0
-        var body: some View {
-            ZStack {
+    // Gets the screen size of the device and changes the size of the buttons
+    let screenSize: CGRect = UIScreen.main.bounds
+    var verticalPadding: CGFloat { screenSize.height * 0.03}
+    var buttonHeightLarge: CGFloat { screenSize.height * 0.50}
+    var buttonHeightSmall: CGFloat { screenSize.height * 0.10}
+    
+    var body: some View {
+        ZStack {
+            VStack() {
+                Spacer()
                 
-                VStack(spacing: CGFloat(verticalPaddingForForm)) {
-                    Text("Welcome To TeckIT")
-                        .font(.title)
-                        .foregroundColor(Color.black)
+                Text("Welcome To TeckIT")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Group {
                     HStack {
                         Image(systemName: "person")
                             .foregroundColor(.secondary)
@@ -41,40 +49,104 @@ struct LogInView: View {
                     .padding()
                     .background(Color.white)
                     .cornerRadius(10)
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Text("Sign Up")
-                            .padding()
-                        
-                    }
-                    .background(Color.black)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(10)
-                    
-                }.padding(.horizontal, CGFloat(verticalPaddingForForm))
+                }
                 
-                struct ContentView: View {
-                               var body: some View {
-                                   SignInWithAppleButton(.signIn, onRequest: configure, onCompletion:handle)
-                                       .frame(height:45)
-                                       .padding()
-                               }
-                               }
-            }
-        };
-    }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+                Button(action: {
+                    print("Name")
+                }) {
+                    Text("Sign Up")
+                        .padding()
+                    
+                }
+                .background(Color.black)
+                .foregroundColor(Color.white)
+                .cornerRadius(10)
+                
+                Spacer()
+                Spacer()
+                
+            }.padding(.horizontal)
         }
-    }
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// Previews
 struct LogInView_Previews: PreviewProvider {
     static var previews: some View {
         LogInView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
+
+
+//struct LogInView: View {
+//
+//    struct ContentView: View {
+//        @State private var name: String = ""
+//        @State private var password: String = ""
+//        let verticalPaddingForForm = 40.0
+//        var body: some View {
+//            ZStack {
+//
+//                VStack(spacing: CGFloat(verticalPaddingForForm)) {
+//                    Text("Welcome To TeckIT")
+//                        .font(.title)
+//                        .foregroundColor(Color.black)
+//                    HStack {
+//                        Image(systemName: "person")
+//                            .foregroundColor(.secondary)
+//                        TextField("Enter your name", text: $name)
+//                            .foregroundColor(Color.black)
+//                    }
+//                    .padding()
+//                    .background(Color.white)
+//                    .cornerRadius(10)
+//
+//                    HStack {
+//                        Image("icons8-key-50")
+//                            .resizable()
+//                            .frame(width: 16.0, height: 16.0) .foregroundColor(.secondary)
+//                        SecureField("Enter password", text: $password)
+//                            .foregroundColor(Color.black)
+//
+//                    }
+//                    .padding()
+//                    .background(Color.white)
+//                    .cornerRadius(10)
+//
+//                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+//                        Text("Sign Up")
+//                            .padding()
+//
+//                    }
+//                    .background(Color.black)
+//                    .foregroundColor(Color.white)
+//                    .cornerRadius(10)
+//
+//                }.padding(.horizontal, CGFloat(verticalPaddingForForm))
+//
+//                struct ContentView: View {
+//                               var body: some View {
+//                                   SignInWithAppleButton(.signIn, onRequest: configure, onCompletion:handle)
+//                                       .frame(height:45)
+//                                       .padding()
+//                               }
+//                               }
+//            }
+//        };
+//    }
+//    struct ContentView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            ContentView()
+//        }
+//    }
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct LogInView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        LogInView()
+//    }
+//}
