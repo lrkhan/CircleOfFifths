88a8b94069f5dee80243da52571fc1347643c75e
LuthforKhan <lrkhan@umich.edu>
2022-02-14 09:59:29 -0500

    labels need to be changes

--- code/TekIT/TekIT/Screens/UserTabView.swift
+++ code/TekIT/TekIT/Screens/UserTabView.swift
@@ -13,8 +13,8 @@ struct UserTabView: View {
     
     var body: some View {
         TabView(selection: $pageSelection) {
-            CommunityView().tabItem { Text("Tab Label 1") }.tag(1)
-            HelpView().tabItem { Text("Tab Label 2") }.tag(2)
+            CommunityView().tabItem { Text("Tab Label 1 change") }.tag(1)
+            HelpView().tabItem { Text("Tab Label 2 change") }.tag(2)
             ProfileView().tabItem { Text("Tab Label 3") }.tag(3)
         }
     }
