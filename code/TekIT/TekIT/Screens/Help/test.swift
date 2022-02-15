ca608ecb0fcf54ffaf685dee3fde5a963732c365
LuthforKhan <lrkhan@umich.edu>
2022-02-15 11:28:44 -0500

    getting the branch started

---
+++ code/TekIT/TekIT/Screens/Help/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  TekIT
+//
+//  Created by Luthfor Khan on 2/15/22.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
