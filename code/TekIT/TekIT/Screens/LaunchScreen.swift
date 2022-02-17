4c50d12e98022fd58e39e1a416ec32f1b88caaa5
Juan <juanrubio081801@gmail.com>
2022-02-17 10:22:03 -0500

    added app icon to each text FIX DESTINATION

--- code/TekIT/TekIT/Screens/LaunchScreen.swift
+++ code/TekIT/TekIT/Screens/LaunchScreen.swift
@@ -38,24 +38,157 @@ struct LaunchScreen: View {
             List{
                 Section(header: Text("Standard Apps")) {
                     //Standard()
-                    NavigationLink("Accessibility", destination: Text("Accessibility Call") )
-                    NavigationLink("Camera", destination:Text("Camera Call"))
-                    NavigationLink("Settings", destination: Text("Settings Call"))
-                    NavigationLink("Safari", destination: Text("Safari Call"))
-                    NavigationLink("Siri", destination: Text("Siri Call") )
-                    NavigationLink("Mail", destination: Text("Mail Call"))
-                    NavigationLink("Messages", destination: Text("Messages Call"))
-                    NavigationLink("Phone", destination: Text("Phone Call"))
-                    NavigationLink("Facetime", destination: Text("FaceTime Call"))
-                    NavigationLink("Clock", destination: Text("Clock Call") )
+                    
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Accessibility")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Accessibility")
+                        }
+                    }
+                        //"Accessibility", destination: Text("Accessibility Call") )
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Camera")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Camera")
+                        }
+                    }
+                    //("Camera", destination:Text("Camera Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Settings")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Settings")
+                        }
+                    }
+                    //("Settings", destination: Text("Settings Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Safari")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Safari")
+                        }
+                    }
+                    //("Safari", destination: Text("Safari Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Siri")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Siri")
+                        }
+                    }
+                    //("Siri", destination: Text("Siri Call") )
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Mail")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Mail")
+                        }
+                    }
+                    //("Mail", destination: Text("Mail Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Messages")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Messages")
+                        }
+                    }
+                    //("Messages", destination: Text("Messages Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Phone")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Phone")
+                        }
+                    }
+                    //("Phone", destination: Text("Phone Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Facetime")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Facetime")
+                        }
+                    }
+                    //("Facetime", destination: Text("FaceTime Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Clock")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Clock")
+                        }
+                    }
+                    //("Clock", destination: Text("Clock Call") )
+                    
+
                     
                 }
                
                 Section(header: Text("Other Apps")) {
                     //OtherApps()
-                    NavigationLink("FaceBook", destination: Text("FaceBook Call") )
-                    NavigationLink("YouTube", destination: Text("YouTube Call"))
-                    NavigationLink("Zoom", destination: Text("Zoom Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Facebook")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Facebook")
+                        }
+                    }
+                    //("FaceBook", destination: Text("FaceBook Call") )
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Youtube")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("YouTube")
+                        }
+                    }
+                    //("YouTube", destination: Text("YouTube Call"))
+                    NavigationLink {
+                        //add something
+                    } label: {
+                        HStack{
+                            Image("Zoom")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Zoom")
+                        }
+                    }
+                    //("Zoom", destination: Text("Zoom Call"))
                     NavigationLink("Other Apps", destination: Text("Other Apps Call") )
                     
                 }
@@ -64,7 +197,7 @@ struct LaunchScreen: View {
                
                     
                     
-                }.navigationTitle("Specific Help ")
+                }.navigationTitle("Select App")
                
         }
             }
